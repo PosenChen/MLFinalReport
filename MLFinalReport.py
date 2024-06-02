82d9fbeed8a23457803f4dad4881e823660b5d76
PosenChen <hjpplus@gmail.com>
2024-06-02 13:54:08 +0800

    參數設定改以手動input方式輸入 等大改版

--- MLFinalReport.py
+++ MLFinalReport.py
@@ -1,3 +1,4 @@
+#%% 更新說明
 """
 Created on Wed May 29 22:22:47 2024
 @author: Posen Chen
@@ -5,8 +6,12 @@ Created on Wed May 29 22:22:47 2024
 Ver 0.1 從老師提供的程式碼裡剪貼出必要的部分 使用CIFAR-10資料集 調整參數讓程式可順利運行
 Ver 0.2 增加簡單計算訓練時間的code 以及刪除修改一些註解
 Ver 0.3 超參數置頂 結果可輸出至CSV 但表格內容還沒設計好
+Ver 0.4 大改版 改用使用者手動輸入參數來增加測試彈性 卷基層數目參數設定說明 增加參數量的計算 表格內容更新 #測試傳說中的LaNet-5{'model_loss': 1.3637713193893433, 'model_acc': 50.78873801916933}訓練時間： 31.333121061325073
 
-模型參數調整計算公式
+將收集到的時間成本資料y 超參數x... 拿去跑迴歸
+觀察不同CNN架構  的時間成本資料 與正確率資料  看看什麼樣的架構的效率較佳  或者前期較佳 後期較佳 與要辨識的目標有什麼關係 能感覺出可能有什麼樣的關係
+殘差分析XD 觀察錯誤辨識的情形 是否常出現在某些類別 Q:若只在少數類別間辨識較差ex:小白鷺跟別的白鷺絲 可否能設計加強辨識難點的訓練資料 或者是在目前已訓練完的模型的基礎上再補訓練要加強的部分
+
 結果繪圖
 迴圈 想要一次跑多組 超難寫 哭哭
 測試其他參數
@@ -17,31 +22,32 @@ Ver 0.3 超參數置頂 結果可輸出至CSV 但表格內容還沒設計好
 (3)卷積層filter的數目.
 (4)pooling的size
 """
+#%% 載入套件&超參數設定區  超參數僅需設定 CRPF 以大寫英文字母CRPF字串方式設定 並修改第153 154行程式碼後 就可以執行全部的程式碼 再自行手動input參數
 import time # 量測量時間成本用
 import torch
 from torch import nn
-# import torchvision
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import pandas as pd
 import os
 
-''' 
-超參數設定區    為方便調整參數 將參數們集中置頂
-''' # 卷積層的數目 原則：1.第一層為C 2.最後為F 3.F前面為P 4.C後面必接R(因為本次不測試不同的activation function) 5.R後面為C或P
+# 卷積層的數目 原則：1.第一層為C 2.F後面只能為F或者放最後 3.第一個F前面為P 4.C後面必接R(因為本次不測試不同的activation function) 5.R後面為C或P
+# 在nn.Sequential()中物件的數量 就是CRP的總數 如 CRCRPF就  nn.Sequential( sb[0],sb[1],sb[2],sb[3],sb[4] )  ,  nn.Sequential( sc[0],sc[1] )
 CRPF = "CRCRPF" #, 4["CRPF"], 6["CRCRPF"], 7["CRPCRPF"], 8["CRCRCRPF"], 9["CRPCRCRPF", "CRCRPCRPF"] , 10["CRCRCRCRPF", "CRPCRPCRPF"]
-CKS = 3 # [3, 5, 7] # kernel_size= 也就是卷積層filter 大小
-CS = 1 # [1, 2, 3] # stride= 每次filter移動的步數
-CP = 1 # [0, 1, 2] # padding= 圖片外圍多鋪幾層
-PKS = 3 # [3, 4, 5] # nn.MaxPool2d(kernel_size= ) 也就是pooling的大小
-PS = 4 # [1, 2, 3, 4, 5] # nn.MaxPool2d(預設的stride=kernel_size )  # 不在這次報告的測試範圍 就用4
+print("請153行程式碼 self.block_1 = nn.Sequential(  )  的括弧中填入",CRPF.count("C") + CRPF.count("R") + CRPF.count("P"),"個物件")
+print("請在154行程式碼 self.classifier = nn.Sequential(  )  的括弧中填入",CRPF.count("F")+1,"個物件")
+# CKS = 3 # [3, 5, 7] # kernel_size= 也就是卷積層filter 大小
+# CS = 1 # [1, 2, 3] # stride= 每次filter移動的步數
+# CP = 1 # [0, 1, 2] # padding= 圖片外圍多鋪幾層
+# PKS = 4 # [3, 4, 5] # nn.MaxPool2d(kernel_size= ) 也就是pooling的大小
+# PS = 4 # [1, 2, 3, 4, 5] # nn.MaxPool2d(預設的stride=kernel_size )  # 不在這次報告的測試範圍 就用4
 epochs = 3 # 總共跑幾次epochs # 不在這次報告的測試範圍 就用3
 BATCH_SIZE = 32 # 設定batch size  每次forward back抓幾筆資料來訓練  # 不在這次報告的測試範圍 就用32
 HU = 10 # 設定每次convolution後output_channels的數量  # 不在這次報告的測試範圍 就用10
 LR = 0.1 # 學習率  # 不在這次報告的測試範圍 就用0.1
 # 不同的optimization方法
-
-# 使用內建資料集 CIFAR10
+#%% 資料處理 & 設定batch_size
+# 使用內建資料集 CIFAR10 (因為較容易帶入範例程式碼)
 # 訓練資料
 train_data = datasets.CIFAR10(
     root="data", # 資料位於工作資料夾內的data資料夾
@@ -74,42 +80,78 @@ test_dataloader = DataLoader(test_data,
     batch_size=BATCH_SIZE,
     shuffle=False # don't necessarily have to shuffle the testing data
 )
+#%% CNN 模型規劃區
+sb = [] # nn.Sequential for self.Block_1
+sc = [] # nn.Sequential for self.classifier
+sc.append(nn.Flatten())
+Fcount = 0
+CNNdescribe = []
 
-## CNN
-seq = []
+print("建立總共",len(CRPF),"層的CNN，輸入層為彩色圖片32*32*3，這是第C 1 層：")
+OC = eval(input("out_channels="))
+KS = eval(input("kernel_size="))
+CS = eval(input("stride="))
+CP = eval(input("padding="))
+PR = (KS*KS*3+1)*OC # 參數量計算
+sb.append(nn.Conv2d(in_channels=3,out_channels=HU,kernel_size=KS,stride=CS,padding=CP))
+IC = OC # 下一層的in_channels
+HW = 32 + 2*CP - KS + 1 # 下一層的Height Width
+CNNdescribe.append(["CNN模型：", CRPF, "輸入層為彩色圖片32*32*3 \n 第,1,層為卷積層 kernel_size=", KS, "輸出為",HW,"*",HW,"*",OC,"\n"])
 
-for i in range(len(CRPF)):
-    if i == 0:
-        seq.append(nn.Conv2d(in_channels=3,out_channels=HU,kernel_size=CKS,stride=CS,padding=CP))
-        continue
-    elif CRPF[i] == "C":
-        seq.append(nn.Conv2d(in_channels=HU,out_channels=HU,kernel_size=CKS,stride=CS,padding=CP))
+for i in range(1,len(CRPF)):
+    if CRPF[i] == "C":
+        print("建立總共",len(CRPF),"層的CNN，上一層傳入",HW,"*",HW,"*",IC, " ，這是第C",i+1,"層：")
+        OC = eval(input("out_channels="))
+        KS = eval(input("kernel_size="))
+        CS = eval(input("stride="))
+        CP = eval(input("padding="))
+        PR = PR + (KS*KS*3+1)*OC # 參數量計算
+        sb.append(nn.Conv2d(in_channels=IC,out_channels=HU,kernel_size=KS,stride=CS,padding=CP))
+        IC = OC # 下一層的in_channels
+        HW = HW + 2*CP - KS + 1 # 下一層的Height Width
+        CNNdescribe.append(["第",i+1,"層為卷積層 kernel_size=", KS, "輸出為",HW,"*",HW,"*",OC,"\n"])
     elif CRPF[i] == "R":
-        seq.append(nn.ReLU())
+        sb.append(nn.ReLU())
+        CNNdescribe.append(["第",i+1,"層為ReLU層\n"])
     elif CRPF[i] == "P":
-        seq.append(nn.MaxPool2d(kernel_size=4)),
+        print("建立總共",len(CRPF),"層的CNN，上一層傳入",HW,"*",HW,"*",IC, " ，這是第P",i+1,"層：")
+        KS = eval(input("kernel_size="))
+        sb.append(nn.MaxPool2d(kernel_size=KS))
+        HW = HW / KS # 下一層的Height Width
+        CNNdescribe.append(["第",i+1,"層為池化層 使用MaxPool, kernel_size=", KS, "輸出為",HW,"*",HW,"*",OC,"\n"])
+    elif CRPF[i] == "F" :
+        if Fcount == 0:
+            INF = int(HW*HW*IC)
+            Fcount += 1
+        if i == len(CRPF)-1:
+            PR = PR + (INF+1)*10
+            sc.append(nn.Linear(in_features=INF, out_features=10))
+            CNNdescribe.append(["第",i+1,"層為全連接層 , 輸出feature數為10個\n", "整個模型的參數量為", PR])
+        else:
+            print("建立總共",len(CRPF),"層的CNN，上一層傳入",INF,"個features，這是第F",i+1,"層：")
+            OF = eval(input("out_features="))
+            PR = PR + (INF+1)*OF
+            sc.append(nn.Linear(in_features=INF, out_features=OF))
+            INF = OF
+            CNNdescribe.append(["第",i+1,"層為全連接層 , 輸出feature數為",OF,"個\n"])        
     else:
         continue
 
-# CRPF.count("C")
-# outputw = 32 + CRPF.count("C") * ( 2 * CP - CKS + 1)  # 每經過一層C的大小改變量 一開始的圖片大小為32
-# if outputw % == 0:
-    
-# LS = outputw / PKS
-
+# paste1 = []
+# paste2 = []
+# for j in range(CRPF.count("C") + CRPF.count("R") + CRPF.count("P")):
+#     paste1.append("sb["+str(j)+"]")
+# for j in range(CRPF.count("F")):
+#     paste2.append("sb["+str(j)+"]")
+# print(paste1,"\n",paste2)
+#%% CNN
 #設計CNN藍圖
 class CIFAR10V1(nn.Module):
 
-    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+    def __init__(self):
         super().__init__()
-        self.block_1 = nn.Sequential(seq[0],seq[1],seq[2],seq[3],seq[4])
-
-        self.classifier = nn.Sequential(
-            nn.Flatten(),# DENSE LAYER
-            # Where did this in_features shape come from? 
-            nn.Linear(in_features=hidden_units*8*8, 
-                      out_features=output_shape) # output_shape = 共幾類  這種output在 pytorch裡叫 logit
-        ) # 因為  nn.CrossEntropyLoss() 的input 用的是 "unnormalized" logits  所以不用在加nn.sigmoid() 不用 normalized 
+        self.block_1 = nn.Sequential( sb[0],sb[1],sb[2],sb[3],sb[4] )
+        self.classifier = nn.Sequential( sc[0],sc[1] )
     
     def forward(self, x: torch.Tensor):
         x = self.block_1(x)
@@ -117,9 +159,7 @@ class CIFAR10V1(nn.Module):
         return x
 
 torch.manual_seed(42)
-model_1 = CIFAR10V1(input_shape=3, 
-    hidden_units = HU, 
-    output_shape = len(class_names))
+model_1 = CIFAR10V1()
 
 # Setup loss and optimizer  
 loss_fn = nn.CrossEntropyLoss() #    nn.CrossEntropyLoss() 用在 機率 或 類別
@@ -218,21 +258,34 @@ print(model_1)
 print(model_1_results)
 print("訓練時間：",total_time)
 
-#df1 = pd.Series(CRPF) # 未完成
-# df2 = pd.Series(total_time)
-df3 = pd.DataFrame(model_1_results, index=[0]).T
-
-
 # to_csv
-# listRMSE = []
-# listTime = []
-
-#                 listRMSE.append([i,j,k,l,"RMSE=", RMSE_Test16.values, "\n"])
-#                 listTime.append([i,j,k,l,"Time=", total_time, "\n"])
-
-# df4 = pd.concat(df2, df3, axis=0)
+df1 = pd.DataFrame(CNNdescribe)
+df2 = pd.DataFrame(model_1_results, index=[0])
+df2["training_time"] = total_time
+df3 = pd.concat([df1, df2], axis=0)
 csv_file_path = 'CNN結果輸出.csv'
 if os.path.exists(csv_file_path):
     df3.to_csv(csv_file_path, mode='a', header=False, index=False)
 else:
     df3.to_csv(csv_file_path, mode='w', header=True, index=False)
+
+#%% 雜記
+# # hello world  傳說中的LeNet-5 將Sigmoid() 改成ReLU()的版本
+#         self.block_1 = nn.Sequential(
+#             nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5,stride=1,padding=0), # 32 -> 28
+#             nn.ReLU(),
+#             nn.MaxPool2d(kernel_size=2), # 28 -> 14
+#             nn.Conv2d(in_channels=6,out_channels=16,kernel_size=5,stride=1,padding=0), # 14 -> 10
+#             nn.ReLU(),
+#             nn.MaxPool2d(kernel_size=2), # 10 ->5
+#             )
+
+#         self.classifier = nn.Sequential(
+#             nn.Flatten(),# DENSE LAYER
+#             nn.Linear(in_features=16*5*5, out_features=120),
+#             nn.ReLU(),
+#             nn.Linear(in_features=120, out_features=84),
+#             nn.ReLU(),
+#             nn.Linear(in_features=84,out_features=output_shape)
+#             ) 
+
